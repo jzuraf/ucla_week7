670eb249739573d84c556042b456b7edee168d03
jzuraf <josephzuraf@gmail.com>
2021-05-19 17:49:01 -0400

    Completed tutorial

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,32 @@
 jQuery(function() {
 
+	let my_switch = false;
+
 	$('#my_button').click(function () {
+
+		if ( my_switch == false ) {
+			my_switch = true;
+
+			$(this).html('Click for FREE TRIP!').removeClass('btn-primary').addClass('btn-success');
+
+			$('#my_image').attr('src', 'images/puppy.png');
+		} else {
+			my_switch = false;
+
+			$(this).html('Click for FREE CRUISE!').removeClass('btn-success').addClass('btn-primary');
+
+			$('#my_image').attr('src', 'images/kitten.png');
+		}
+
+		// if ( $('.square.alt').hasClass('red') ) {
+		// 	$('.square.alt').removeClass('red');
+		// } else {
+		// 	$('.square.alt').addClass('red');
+		// }
+
+
+
+
 	});
 
 });
