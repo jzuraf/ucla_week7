dc7e65bbe1de9e6904ce6c3204589e16365fccb8
jzuraf <josephzuraf@gmail.com>
2021-05-19 20:32:53 -0400

    Congratulations

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,21 @@ jQuery(function() {
 			my_switch = true;
 
 			$(this).html('Click for FREE TRIP!').removeClass('btn-primary').addClass('btn-success');
+			$('#prize').html('You won a free cruise to Alaska!')
+			$('#my_image').attr('src', 'images/alaska.png');
+			$('#mini').attr('src', 'images/boat.png');
 
-			$('#my_image').attr('src', 'images/puppy.png');
+
+			//$('.body').css("background-image", "url('../images.alaska.png')");
 		} else {
 			my_switch = false;
 
 			$(this).html('Click for FREE CRUISE!').removeClass('btn-success').addClass('btn-primary');
+			$('#prize').html('You won a free trip to Hawaii!')
+			$('#my_image').attr('src', 'images/hawaii.png');
+			$('#mini').attr('src', 'images/plane.png');
 
-			$('#my_image').attr('src', 'images/kitten.png');
+			//$('.body').css("background-image", "url('../images/hawaii.png')");
 		}
 
 		// if ( $('.square.alt').hasClass('red') ) {
